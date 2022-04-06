08208d8bda1456e1b96617c37515923d7b7a086a
Eugene kodzila <simba.eugene@gmail.com>
2022-04-06 13:35:00 +0300

    Initial Commit

---
+++ TestWorkSiberian/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWorkSiberian
+//
+//  Created by Наташа on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
