ff08ee2a8e53655e45c6dfc77763c9beeb6b66bc
Eugene kodzila <simba.eugene@gmail.com>
2022-04-15 01:40:35 +0300

    1)Исправил свич при помощи кода, теперь в неактивном состоянии видно границы 2)Добавил новый вьюконтроллер и изменил ему цвет 3)Протянул от кнопки ВОЙТИ секвой шоу до нового вьюконтроллера 4)Добавил первому вьюконтроллеру юайнавгейшенконтроллер 5)новый вьюконтроллер перенес в новый сторибордчерез редактор и назвал его листвью

--- TestWorkSiberian/ViewController.swift
+++ TestWorkSiberian/ViewController.swift
@@ -7,9 +7,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switcher: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
        print("Привет Женя, все работает")
+        switcher.isOn = false
+        switcher.onTintColor = UIColor.red
+        switcher.thumbTintColor = UIColor.black
+        switcher.subviews[0].subviews[0].backgroundColor = UIColor.white
+        
     }
   
 }
