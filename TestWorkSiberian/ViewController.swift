ec3be6b9cc90343c59ee41881fd56a7140a3de53
Eugene kodzila <simba.eugene@gmail.com>
2022-04-15 23:56:28 +0300

    1)Пробросил с main storyboard к view controller IBOutlet всех элементов интерфейса 2)Пробросил с main storyboard к view controller IBActiont элементов Button,Switch 3)В классе ViewController создал enum Constans в который внес все тексты элементов 4)С помощью didSet установил contentMode и image для UIImageView 5)С помощью didSet установил text для UILabel 6)С помощью didSet установил placeholder для UITextField 7)C помощью didSet установил title и titleColor для UIButton 8)Очистил все текстовые поля в Main.storyboard 9)Запустил приложение

--- TestWorkSiberian/ViewController.swift
+++ TestWorkSiberian/ViewController.swift
@@ -6,8 +6,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Constants {
+        static let mainTitle = "Мое приложение"
+        static let termsTitle = "Согласен с условиями"
+        static let loginPlaceholder = "Введите логин"
+        static let passwordPlaceholder = "Введите пароль"
+        static let enterTitle = "Войти"
+    }
 
     @IBOutlet weak var switcher: UISwitch!
+    
+    @IBOutlet weak var backgroundImageView: UIImageView! {
+        didSet {
+            backgroundImageView.contentMode = .scaleAspectFill
+            backgroundImageView.image = UIImage(named: "background")
+        }
+    }
+    
+    @IBOutlet weak var mainLabel: UILabel! {
+        didSet {
+            mainLabel.text = Constants.mainTitle
+        }
+    }
+    
+    @IBOutlet weak var loginTextField: UITextField! {
+        didSet {
+            loginTextField.placeholder = Constants.loginPlaceholder
+        }
+    }
+    
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet {
+            passwordTextField.placeholder = Constants.passwordPlaceholder
+        }
+    }
+    
+    @IBOutlet weak var enterButton: UIButton! {
+        didSet {
+            enterButton.setTitleColor(.black, for: .normal)
+            enterButton.setTitle(Constants.enterTitle, for: .normal)
+        }
+    }
+        
+    @IBOutlet weak var termsLabel: UILabel! {
+        didSet {
+            termsLabel.text = Constants.termsTitle
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        print("Привет Женя, все работает")
@@ -15,7 +62,15 @@ class ViewController: UIViewController {
         
     }
   
-    func setupSwitch() {
+    
+    @IBAction func enterButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func acceptTermsSwitch(_ sender: Any) {
+    }
+    
+    
+    private func setupSwitch() {
         switcher.isOn = false
         switcher.onTintColor = UIColor.red
         switcher.thumbTintColor = UIColor.black
