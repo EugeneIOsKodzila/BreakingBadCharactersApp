3ded1643bf6cb876b97d55d5ac7f1ec4a1d39bc1
Eugene kodzila <simba.eugene@gmail.com>
2022-04-15 12:06:38 +0300

    вынес настройки свича в отдельную функцию

--- TestWorkSiberian/ViewController.swift
+++ TestWorkSiberian/ViewController.swift
@@ -11,12 +11,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        print("Привет Женя, все работает")
+        setupSwitch()
+        
+    }
+  
+    func setupSwitch() {
         switcher.isOn = false
         switcher.onTintColor = UIColor.red
         switcher.thumbTintColor = UIColor.black
         switcher.subviews[0].subviews[0].backgroundColor = UIColor.white
-        
     }
-  
+    
 }
 
