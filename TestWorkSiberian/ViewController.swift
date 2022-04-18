f4ac119f5042b913355b2466cf576a1710e2645d
Eugene kodzila <simba.eugene@gmail.com>
2022-04-18 12:50:26 +0300

    исправил отступы

--- TestWorkSiberian/ViewController.swift
+++ TestWorkSiberian/ViewController.swift
@@ -16,39 +16,33 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var switcher: UISwitch!
-    
     @IBOutlet weak var backgroundImageView: UIImageView! {
         didSet {
             backgroundImageView.contentMode = .scaleAspectFill
             backgroundImageView.image = UIImage(named: "background")
         }
     }
-    
     @IBOutlet weak var mainLabel: UILabel! {
         didSet {
             mainLabel.text = Constants.mainTitle
         }
     }
-    
     @IBOutlet weak var loginTextField: UITextField! {
         didSet {
             loginTextField.placeholder = Constants.loginPlaceholder
         }
     }
-    
     @IBOutlet weak var passwordTextField: UITextField! {
         didSet {
             passwordTextField.placeholder = Constants.passwordPlaceholder
         }
     }
-    
     @IBOutlet weak var enterButton: UIButton! {
         didSet {
             enterButton.setTitleColor(.black, for: .normal)
             enterButton.setTitle(Constants.enterTitle, for: .normal)
         }
     }
-        
     @IBOutlet weak var termsLabel: UILabel! {
         didSet {
             termsLabel.text = Constants.termsTitle
@@ -59,16 +53,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
        print("Привет Женя, все работает")
         setupSwitch()
-        
     }
   
-    
     @IBAction func enterButtonPressed(_ sender: Any) {
     }
-    
     @IBAction func acceptTermsSwitch(_ sender: Any) {
     }
-    
     
     private func setupSwitch() {
         switcher.isOn = false
@@ -76,6 +66,5 @@ class ViewController: UIViewController {
         switcher.thumbTintColor = UIColor.black
         switcher.subviews[0].subviews[0].backgroundColor = UIColor.white
     }
-    
 }
 
