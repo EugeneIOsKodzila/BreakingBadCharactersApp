30e12f6eb8100ac79b99f8e55cfe613074d5f0de
Eugene kodzila <simba.eugene@gmail.com>
2022-05-06 09:07:02 +0300

    1)Сделал extension  ListTableViewController где унаследовал делегаты UITableViewDataSource, UITableViewDelegate 2)Реализовал обязательные функции делегат 3)В методе numberOfRowsInSection вернул 10 ячеек 4)В методе cellForRowAt вернул ячейку ListTableViewCell 5)Сделал IBOutlet для таблицы 6)Указал этой таблице delegate, dataSource 7)Зарегистрировал ячейку для таблицы 8)Создал extension для UITableViewCell, где вернул сокращенную запись nib и identifier 9)Запустил приложение, перешел на второй экран.

--- TestWorkSiberian/Flow/List/ListViewController.swift
+++ TestWorkSiberian/Flow/List/ListViewController.swift
@@ -9,5 +9,30 @@ import Foundation
 import UIKit
 
 class ListViewController: UIViewController {
+    @IBOutlet weak var listTableView: UITableView! {
+        didSet {
+            listTableView?.dataSource = self
+            listTableView?.delegate = self
+            listTableView?.register(ListTableViewCell.nib, forCellReuseIdentifier: ListTableViewCell.identifier)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as! ListTableViewCell
+        
+        return cell
+    }
+    
     
 }
