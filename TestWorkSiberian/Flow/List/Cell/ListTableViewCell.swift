0000552507a85ca8a60bd7c5d14cc62030399b15
Eugene kodzila <simba.eugene@gmail.com>
2022-05-17 12:17:26 +0300

    1)сделал настройки в ячейке 2)добавил активити индикатор

--- TestWorkSiberian/Flow/List/Cell/ListTableViewCell.swift
+++ TestWorkSiberian/Flow/List/Cell/ListTableViewCell.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class ListTableViewCell: UITableViewCell {
-    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoImageView: UIImageView! {
+        didSet {
+            activityIndicatorView.startAnimating()
+            activityIndicatorView.isHidden = false
+        }
+    }
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     
     func userInfoRequest(userInfo: BreakingBadCharacters?) {
         guard let userInfo = userInfo else { return }
         if let imageUrl = userInfo.img {
             photoImageView.loadFrom(URLAddress: imageUrl)
+            activityIndicatorView.stopAnimating()
+            activityIndicatorView.isHidden = true
         }
         nameLabel.text = userInfo.nickname
+        layer.borderWidth = CGFloat(1.5)
+        layer.borderColor = UIColor.white.cgColor
+        photoImageView.contentMode = .scaleAspectFill
+        
     }
 }
