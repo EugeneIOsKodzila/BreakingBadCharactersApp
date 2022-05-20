356202182ca37b1ec4bbb56598c4d1e560119346
Eugene kodzila <simba.eugene@gmail.com>
2022-05-20 11:30:10 +0300

    исправил названия функций

--- TestWorkSiberian/Flow/List/Cell/ListTableViewCell.swift
+++ TestWorkSiberian/Flow/List/Cell/ListTableViewCell.swift
@@ -12,17 +12,17 @@ class ListTableViewCell: UITableViewCell {
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     
-    func userInfoRequest(userInfo: BreakingBadCharacters?) {
+    func set(userInfo: BreakingBadCharacters?) {
         guard let userInfo = userInfo else { return }
         if let imageUrl = userInfo.img {
             photoImageView.kf.indicatorType = .activity
             photoImageView.kf.setImage(with: URL(string:imageUrl), placeholder: nil, options: [.transition(.fade(0.7))], completionHandler: nil)
         }
         nameLabel.text = userInfo.nickname
-        cellAddon()
+        setupCell()
     }
     
-    func cellAddon() {
+    func setupCell() {
         layer.borderWidth = CGFloat(1.5)
         layer.borderColor = UIColor.white.cgColor
         photoImageView.contentMode = .scaleAspectFill
