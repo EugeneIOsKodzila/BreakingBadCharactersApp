f30dcb4152c4fdb40bff5a3f664d0aa8beb8abcb
Eugene kodzila <simba.eugene@gmail.com>
2022-06-06 07:52:03 +0300

    1)На экране с таблицей сверху таблицы расположил SearchBar 2)Реализовал функцию поиска данных полученных из прошлого задания. При введении любого символа производится поиск 3)Если ничего не нашлось вывожу об этом информацию в виде UILabel с надписью в середине экрана 4)При нажатии на нативную кнопку “Отмена” поиск сбрасывается и отображаются все данные в таблице 5)Добавил функцию работы с локальным файлом json

--- TestWorkSiberian/Flow/List/ListViewController.swift
+++ TestWorkSiberian/Flow/List/ListViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class ListViewController: UIViewController {
     let networkService = NetworkService()
     var breakingBadCharacters: [BreakingBadCharacters] = []
+    var filteredCharacters = [BreakingBadCharacters]()
+    let searchController = UISearchController(searchResultsController: nil)
+    var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
     
     @IBOutlet weak var listTableView: UITableView! {
         didSet {
@@ -18,10 +24,12 @@ class ListViewController: UIViewController {
             listTableView?.delegate = self
             listTableView?.register(ListTableViewCell.nib, forCellReuseIdentifier: ListTableViewCell.identifier)
         }
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSearchBar()
         requestBreakingBad()
     }
     
@@ -33,21 +41,78 @@ class ListViewController: UIViewController {
                 self.breakingBadCharacters = breakingbadCharacters
                 self.listTableView.reloadData()
             case .failure(let error):
+                self?.fetchData()
                 print("error", error)
             }
         }
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        searchController.searchBar.delegate = self
+        navigationController?.navigationBar.prefersLargeTitles = true
+        searchController.obscuresBackgroundDuringPresentation = false
+    }
+    
+    func fetchData(){
+        guard let fileLocation = Bundle.main.url(forResource: "localDataBB", withExtension: "json") else { return }
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            let receivedData = try JSONDecoder().decode([BreakingBadCharacters].self, from: data)
+            self.breakingBadCharacters = receivedData
+            DispatchQueue.main.async {
+                self.listTableView.reloadData()
+            }
+        } catch {
+            print("Error while decoding JSON")
+        }
+    }
+    
+    func setupNoDataLabel() {
+        let emptyLabel = UILabel()
+        emptyLabel.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
+        emptyLabel.text = "Ooooops no result"
+        emptyLabel.textAlignment = NSTextAlignment.center
+        self.listTableView.backgroundView = emptyLabel
     }
 }
 
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return breakingBadCharacters.count
+        if searchBarIsEmpty {
+            return breakingBadCharacters.count
+        }
+        if tableView.visibleCells.isEmpty {
+            setupNoDataLabel()
+        } else {
+            self.listTableView.backgroundView = nil
+        }
+        return filteredCharacters.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as! ListTableViewCell
-        let userInfo = breakingBadCharacters[indexPath.row]
+        if searchBarIsEmpty {
+            let userInfo = breakingBadCharacters[indexPath.row]
+            cell.set(userInfo: userInfo)
+            return cell
+        }
+        let userInfo = filteredCharacters[indexPath.row]
         cell.set(userInfo: userInfo)
         return cell
+    }
+}
+
+extension ListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredCharacters = searchText.isEmpty ? breakingBadCharacters:breakingBadCharacters.filter {
+            (item: BreakingBadCharacters) -> Bool in return item.name.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        listTableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        listTableView.reloadData()
     }
 }
